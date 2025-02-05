4f1b814f2def4797193a0950024bc51081bd74dc
Ruthvik Yogesh Joshi <ruthvikyogeshjoshi@Ruthviks-MacBook-Air-4.local>
2025-02-05 15:13:58 +0530

    02_Variables

---
+++ 02_Variables in Java/variables.java
@@ -0,0 +1,23 @@
+public class variables {
+    public static void main(String args[]){
+        String name = "tony stark";
+        System.out.println(name);
+
+        int a=25;
+        int b=10;
+    
+        int sum = a+b;
+    
+        int product = a*b;
+
+        name = "IronMan";
+
+        System.out.println(name);
+    
+        System.out.println(sum);
+        System.out.println(a+b);
+        System.out.println(product);
+        System.out.println(a*b);
+    }
+
+}
