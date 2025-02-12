ce417b180977c580d0f4f3b8fae47c57d1726f2a
Ruthvik Yogesh Joshi <ryjoshi14@gmail.com>
2025-02-12 22:39:53 +0530

    sixteenth commit

--- 15_Linked List/inbuiltlinkedList.java
+++ 15_Linked List/inbuiltlinkedList.java
@@ -2,6 +2,23 @@ import java.util.*;
 
 public class inbuiltlinkedList {
     public static void main(String[] args) {
-        
+        LinkedList<String> list = new LinkedList<String>();
+        list.addFirst("a");
+        list.addFirst("is");
+        System.out.println(list);
+
+        list.addFirst("This");
+        list.addLast("List");
+        System.out.println(list);
+        System.out.println(list.size());
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i)+" -> ");
+        }
+        System.out.println("NULL");
+
+        list.remove(2);
+        System.out.println(list);
     }
 }
+`
