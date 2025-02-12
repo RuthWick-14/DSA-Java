01a490f9434f302af6c4314153c5b453dd3e38bb
Ruthvik Yogesh Joshi <ryjoshi14@gmail.com>
2025-02-12 21:33:51 +0530

    fifteenth commit

---
+++ 15_Linked List/linkedList.java
@@ -0,0 +1,66 @@
+public class linkedList {
+
+    Node head;
+
+    class Node {
+        String data;
+        Node next;
+
+        Node (String data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // add first or add last
+
+    public void addFirst(String data){
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(String data){
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node currNode = head;
+        if (currNode.next !=  null) {
+            currNode = currNode.next;
+        }
+
+        currNode.next = newNode;
+    }
+
+    //print
+    public void printList() {
+        if (head == null) {
+            System.out.println("List is empty.");
+        }
+        Node currNode = head;
+        while (currNode != null) {
+            System.out.print(currNode.data+ " -> ");
+            currNode = currNode.next;
+        }
+        System.out.println("NULL");
+    }
+    public static void main(String[] args) {
+        linkedList list = new linkedList();
+        list.addFirst("a");
+        list.addFirst("is");
+        list.printList();
+        System.out.println();
+        list.addLast("List");
+        list.printList();
+        list.addFirst("This");
+        list.printList();`
+    }
+}
