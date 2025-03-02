e06451058ebf3ef0e4348fd9bbafefb4a96075ca
Ruthvik Yogesh Joshi <ryjoshi14@gmail.com>
2025-03-02 13:48:54 +0530

    seventeenth commit

--- 16_Stack/pushBottomStack.java
+++ 16_Stack/pushBottomStack.java
@@ -11,12 +11,23 @@ public class pushBottomStack {
         pushBottom(data, s);
         s.push(top);
     }
+
+    public static void reverse(Stack<Integer> s){
+        if (s.isEmpty()) {
+            return;
+        }
+        int top = s.pop();
+        reverse(s);
+        pushBottom(top, s);
+    }
+
     public static void main(String[] args) {
         Stack<Integer> s = new Stack<>();
         s.push(1);
         s.push(2);
         s.push(3);
         pushBottom(4, s);
+        reverse(s);
 
         while (!s.isEmpty()) {
             System.out.println(s.peek());
