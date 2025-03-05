343047f6346ced0969fdcfc0df06ed0ec4c84133
Ruthvik Yogesh Joshi <ryjoshi14@gmail.com>
2025-03-05 23:16:34 +0530

    seventeenth commit

---
+++ 17_Queue/queueCollectionFramework.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class queueCollectionFramework {
+
+     public static void main(String[] args) {
+        // Queue q = new Queue();
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        System.out.println(q.remove());
+        q.add(6);
+        System.out.println(q.remove());
+        q.add(7);
+
+        while (!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+        }
+     }
+
+}
