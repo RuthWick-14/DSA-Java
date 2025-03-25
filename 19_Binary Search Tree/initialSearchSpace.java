9f414ed3091319b7f8bf531b40b226409115ba81
Ruthvik Yogesh Joshi <ryjoshi14@gmail.com>
2025-03-25 18:01:31 +0530

    19

--- 19_Binary Search Tree/initialSearchSpace.java
+++ 19_Binary Search Tree/initialSearchSpace.java
@@ -1,22 +1,41 @@
 public class initialSearchSpace {
-    public static int initialSearch(int arr[], int target, int high, int low){
-        int size = arr.length;
-        low = 0;
-        high = size-1;
+    //using iteration
+    // public static int initialSearch(int arr[], int target, int high, int low){
+    //     int size = arr.length;
+    //     low = 0;
+    //     high = size-1;
 
-        while (high>=low) {
-            int mid = (low+high)/2;
-            if (target == arr[mid]) {
-                return mid;
-            }
-            else if(target > arr[mid]){
-                low = mid + 1;
-            }
-            else {
-                high = mid - 1;
-            }
+    //     while (high>=low) {
+    //         int mid = (low+high)/2;
+    //         if (target == arr[mid]) {
+    //             return mid;
+    //         }
+    //         else if(target > arr[mid]){
+    //             low = mid + 1;
+    //         }
+    //         else {
+    //             high = mid - 1;
+    //         }
+    //     }
+    //     return -1;
+    // }
+
+    // using recursion
+
+    public static int initialSearch(int arr[], int target, int high, int low){
+        int midIndex = (low+high)/2;
+        if (low > high) {
+            return -1;
         }
-        return -1;
+        if (target == arr[midIndex]) {
+            return midIndex;
+        }
+        if (target > arr[midIndex]) {
+            return initialSearch(arr, target, high, midIndex+1);
+        }
+        else { 
+            return initialSearch(arr, target, midIndex-1, low);
+        }
     }
 
     public static void main(String[] args) {
