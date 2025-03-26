0957561f17590f7e92b7b2d2be92309ac0fc8a8f
Ruthvik Yogesh Joshi <ryjoshi14@gmail.com>
2025-03-26 20:33:44 +0530

    nineteenth commit

--- 19_Binary Search Tree/BStwo.java
+++ 19_Binary Search Tree/BStwo.java
@@ -1,25 +1,39 @@
 public class BStwo {
     // using iteration
-    public static int lowerBound(int[] arr, int target) {
-        int low = 0;
-        int high = arr.length - 1;
+    // public static int lowerBound(int[] arr, int target) {
+    //     int low = 0;
+    //     int high = arr.length - 1;
+    //     int ans = arr.length;
+    //     while (low <= high) {
+    //         int mid = (low + high) / 2;
+
+    //         if (target <= arr[mid]) {
+    //             ans = mid;
+    //             high = mid - 1;
+    //         } else {
+    //             low = mid + 1;
+    //         }
+    //     }
+    //     return ans;
+    // }
+
+    // using recursion
+    public static int lowerBound(int arr[], int target, int low, int high){
         int ans = arr.length;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-
-            if (target <= arr[mid]) {
-                ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
+        if(low > high){
+            return low;
         }
-        return ans;
+        int mid = (high+low)/2;
+        if (arr[mid] >= target ) {
+            ans = mid;
+            return lowerBound(arr, target, low, mid-1);
+        }
+        else {
+            return lowerBound(arr, target, mid+1, high);
+        }
     }
-
-    // using recursive
     public static void main(String[] args) {
         int arr[] = { 1, 2, 3, 3, 5, 8, 8, 10, 10, 11 };
-        System.out.println(lowerBound(arr, 6));
+        System.out.println(lowerBound(arr, 4, 0, arr.length-1));
     }
 }
