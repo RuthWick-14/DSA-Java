e8d6941879742277d05cb1abd327f93a7f6dd006
Ruthvik Yogesh Joshi <ryjoshi14@gmail.com>
2025-04-06 16:10:57 +0530

    fifteenth commit

--- 15_Linked List/LLThree.java
+++ 15_Linked List/LLThree.java
@@ -88,6 +88,82 @@ public class LLThree {
         return head;
     }
 
+    private static void deleteNode(Node temp){
+        Node previous = temp.back;
+        Node front = temp.next;
+        if (front == null) {
+            previous.next = null;
+            temp.back = null;
+            return;
+        }
+        previous.next = front;
+        front.back = previous;
+        temp.next = temp.back = null;
+    }
+
+    private static Node insertBeforeHead(Node head, int val){
+        Node newHead = new Node(val, head, null);
+        head.back = newHead;
+        return newHead;
+    }
+
+    private static Node insertAfterLast(Node head, int val){
+        if (head == null) {
+            return new Node(val);
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        Node newNode = new Node(val);
+        temp.next = newNode;
+        newNode.back = temp;
+        return head;
+    }
+
+    private static Node insertBeforeLast(Node head, int val){
+        if (head.next == null) {
+            return insertBeforeHead(head, val);
+        }
+        Node tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+        Node previous = tail.back;
+        Node newNode = new Node(val, tail, previous);
+        previous.next = newNode;
+        tail.back = newNode;
+        return head;
+    }
+
+    private static Node insertBeforeKthElement(Node head, int k, int val){
+        if (k == 1) {
+            return insertBeforeHead(head, val);
+        }
+        Node temp = head;
+        int counter = 0;
+        while (temp != null) {
+            counter++;
+            if (counter == k) {
+                break;
+            }
+            temp = temp.next;
+        }
+        Node previous = temp.back;
+        Node newNode = new Node(val, temp, previous);
+        previous.next = newNode;
+        temp.back = newNode;
+        return head;
+    }
+
+    private static void insertBeforeNode(Node node, int val){
+        Node previous = node.back;
+        Node newNode = new Node(val, node, previous);
+        previous.next = newNode;
+        node.back = newNode;
+        return;
+    }
+
     public static void main(String[] args) {
         int arr[] = {3, 5, 1, 8, 4};
         Node head = convertNodeToDLL(arr);
@@ -97,6 +173,18 @@ public class LLThree {
         head = deleteLast(head);
         printLinkedList(head);
         head = deleteKthElement(head, 2);
+        printLinkedList(head);
+        deleteNode(head.next);
+        printLinkedList(head);
+        head = insertBeforeHead(head, 10); 
+        printLinkedList(head);
+        head = insertAfterLast(head, 13);
+        printLinkedList(head);
+        head = insertBeforeLast(head, 21);
+        printLinkedList(head);
+        head = insertBeforeKthElement(head, 2, 14);
+        printLinkedList(head);
+        insertBeforeNode(head.next.next, 100);
         printLinkedList(head);
     }
 }
