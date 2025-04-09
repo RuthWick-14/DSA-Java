d71d20a25bc905a0f10125ed5ea3e08840638a22
Ruthvik Yogesh Joshi <ryjoshi14@gmail.com>
2025-04-09 23:50:34 +0530

    nineteenth commit

---
+++ 19_Binary Search Tree/BTone.java
@@ -0,0 +1,17 @@
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data1){
+        this.data = data1;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class BTone {
+    public static void main(String[] args) {
+        
+    }
+}
