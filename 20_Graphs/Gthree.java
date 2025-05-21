ca5fb2502a12f3057bf80775dd9b0d639ca10fc3
Ruthvik Yogesh Joshi <ryjoshi14@gmail.com>
2025-05-21 10:48:25 +0530

    twentieth commit

---
+++ 20_Graphs/Gthree.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Gthree {
+    private static void dfs(int node, boolean vis[], ArrayList<ArrayList<Integer>> adj, ArrayList<Integer> ls){
+        vis[node] = true;
+        ls.add(node);
+
+        for(int it : adj.get(node)){
+            if (vis[it] == false) {
+                dfs(it, vis, adj, ls);
+            }
+        }
+    }
+
+    private static ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj){
+        boolean vis[] = new boolean[V+1];
+        vis[0] = true;
+        ArrayList<Integer> ls = new ArrayList<>();
+        dfs(0, vis, adj, ls);
+        return ls; 
+    }
+}
