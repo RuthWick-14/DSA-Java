882116d9a9c0ef367e03aa66b6ef23db49134229
Ruthvik Yogesh Joshi <ryjoshi14@gmail.com>
2025-05-25 21:47:17 +0530

    twentieth commit

---
+++ 20_Graphs/Gnine.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class Gnine {
+    private static boolean dfs(int node, int parent, int[] vis, ArrayList<ArrayList<Integer>> adj){
+        vis[node] = 1;
+        for (int it : adj.get(node)) {
+            if (vis[it] == 0) {
+                if (dfs(it, node, vis, adj) == true) {
+                    return true;
+                }
+                else if (it != parent) return true;
+            }
+        }
+        return false;
+    }
+    // function to detect cycle in an undirected graph
+    public static boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj){
+        int vis[] = new int[V];
+        for (int i = 0; i < V; i++) {
+            if (vis[i] == 0) {
+                if (dfs(i, -1, vis, adj) == true) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
