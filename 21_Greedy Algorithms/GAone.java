d302a630cfa00750a773c54292615d324f7bdb42
Ruthvik Yogesh Joshi <ryjoshi14@gmail.com>
2025-06-29 20:42:30 +0530

    twentyone commit

--- 21_Greedy Algorithms/GAone.java
+++ 21_Greedy Algorithms/GAone.java
@@ -1,4 +1,19 @@
 //ASSIGN COOKIES
+import java.util.Arrays;
+
 public class GAone {
-    
+    public int findContentChildren(int[] g, int[] s) {
+        int n = g.length;
+        int m = s.length;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int l = 0;
+        int r = 0;
+        while (l < m && r < n) {
+            if (g[r] <= s[l])
+                r = r + 1;
+            l = l + 1;
+        }
+        return r;
+    }
 }
