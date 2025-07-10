ab7b335837f1621406ae5aa8b0ae8916ebe6ce4d
Ruthvik Yogesh Joshi <ryjoshi14@gmail.com>
2025-07-10 20:24:48 +0530

    twentysix commit

--- 26_Sliding Window and 2 Pointer/SWTPtwo.java
+++ 26_Sliding Window and 2 Pointer/SWTPtwo.java
@@ -1,7 +1,31 @@
 //longest subarray with sum <= k
 public class SWTPtwo {
     //brute force -> generate all possible subarrays. by using O(n^2) TC and O(1) SC.
+    //----------------------------------------------------------------
     //better approach
+    // public static int maximumLengthOfSubstring(int[] arr, int k) {
+    //     int l = 0;
+    //     int r = 0;
+    //     int sum = 0;
+    //     int maxLen = 0;
+    //     int n = arr.length;
+
+    //     while (r < n) {
+    //         sum = sum = arr[r];
+    //         while (sum > k) {
+    //             sum = sum - arr[l];
+    //             l = l + 1;
+    //         }
+    //         if (sum <= k) {
+    //             maxLen = Math.max(maxLen, r-l+1);
+    //         }
+    //         r = r + 1;
+    //     }
+    //     return maxLen;
+    // }
+    //----------------------------------------------------------------
+
+    //optimal solution
     public static int maximumLengthOfSubstring(int[] arr, int k) {
         int l = 0;
         int r = 0;
@@ -11,7 +35,7 @@ public class SWTPtwo {
 
         while (r < n) {
             sum = sum = arr[r];
-            while (sum > k) {
+            if (sum > k) {
                 sum = sum - arr[l];
                 l = l + 1;
             }
@@ -23,5 +47,4 @@ public class SWTPtwo {
         return maxLen;
     }
 
-    //optimal solution
 }
