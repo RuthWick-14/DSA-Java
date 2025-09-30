27814c8c630fb9b4dd7aff4e13bc160f8bc12a99
Ruthvik Yogesh Joshi <ryjoshi14@gmail.com>
2025-09-30 18:36:45 +0530

    heap

--- 27_Heaps/Heapone.java
+++ 27_Heaps/Heapone.java
@@ -26,15 +26,65 @@ class Heap {
         }
     }
 
+    void deleteFromHeap() {
+        if(size == 0) {
+            System.out.println("Nothing to delete.");
+            return;
+        }
+        //Step-1 :-> Put last element into first index
+        arr[1] = arr[size];
+        size--;
+
+        //Step-2 :-> take node to its correct position
+        int i = 1;
+        while (i <= size) {
+            int leftIdx = 2*i;
+            int rightIdx = 2*i+1;
+
+            if(leftIdx < size && arr[i] < arr[leftIdx]) {
+                int temp = arr[i];
+                arr[i] = arr[leftIdx];
+                arr[leftIdx] = temp;
+                i = leftIdx;
+            }
+            else if(rightIdx < size && arr[i] < arr[rightIdx]) {
+                int temp = arr[i];
+                arr[i] = arr[rightIdx];
+                arr[rightIdx] = temp;
+                i = rightIdx;
+            }
+            else return;
+        }
+    }
+
     void print() {
-        for (int i = 1; i < arr.length; i++) {
-            if(arr[i] != 0) System.out.print(arr[i]+" ");
+        for (int i = 1; i <= size; i++) {
+            System.out.print(arr[i]+" ");
         }
         System.out.println();
     }
 }
 
 public class Heapone {
+    public static void heapify(int arr[], int n, int i) {
+        int largest = i;
+        int left = 2*i;
+        int right = 2*i+1;
+
+        if (left < n && arr[largest] < arr[left]) {
+            largest = left;
+        }
+        if (right < n && arr[largest] < arr[right]) {
+            largest = right;
+        }
+        if(largest != i) {
+            int temp = arr[largest];
+            arr[largest] = arr[i];
+            arr[i] = temp;
+            heapify(arr, n, largest);
+        }
+    }
+
     public static void main(String[] args) {
         Heap hp = new Heap();
         hp.insert(50);
@@ -43,5 +93,17 @@ public class Heapone {
         hp.insert(52);
         hp.insert(54);
         hp.print();
+        hp.deleteFromHeap();
+        hp.print();
+
+        int arr[] = {-1, 54, 53, 55, 52, 50};
+        int n = 5;
+        for (int i = n/2; i > 0; i--) {
+            heapify(arr, n, i);
+        }
+        for (int i = 1; i <= n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
 }
