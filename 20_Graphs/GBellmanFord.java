cbea786e0cf0c5315aaf5865a5571ab56bfb203e
Ruthvik Yogesh Joshi <ryjoshi14@gmail.com>
2026-02-03 23:22:39 +0530

    Add Bellman-Ford algorithm implementation

---
+++ 20_Graphs/GBellmanFord.java
@@ -0,0 +1,31 @@
+public class GBellmanFord {
+    public int[] bellmanFord(int V, int[][] edges, int src) {
+        // code here
+        int dist[] = new int[V];
+        for(int i = 0; i < V; i++) dist[i] = (int)(1e8);
+        dist[src] = 0;
+        // V x E
+        for(int i = 0; i < V; i++) {
+            for(int[] it : edges) {
+                int u = it[0];
+                int v = it[1];
+                int wt = it[2];
+                if(dist[u] != 1e8 && dist[u] + wt < dist[v]) {
+                    dist[v] = dist[u] + wt;
+                }
+            }
+        }
+        // Nth relaxation to check negative cycle
+        for(int[] it : edges) {
+            int u = it[0];
+            int v = it[1];
+            int wt = it[2];
+            if(dist[u] != 1e8 && dist[u] + wt < dist[v]) {
+                int temp[] = new int[1];
+                temp[0] = -1;
+                return temp;
+            }
+        }
+        return dist;
+    }
+}
